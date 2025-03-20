3339ecd44d209890d47d7af5cc36223f591a1b84
wxm <2801650811@qq.com>
2025-03-20 21:49:24 +0800

    init

---
+++ src/global.h
@@ -0,0 +1,156 @@
+// 预编译
+// ===================================================================================
+#include <cstdio>
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+// 宏定义
+// ===================================================================================
+
+#define MAX_DISK_NUM (10 + 1)
+#define MAX_DISK_SIZE (16384 + 1)
+#define MAX_REQUEST_NUM (30000000 + 1)
+#define MAX_OBJECT_NUM (100000 + 1)
+#define REP_NUM (3)
+#define FRE_PER_SLICING (1800)
+#define EXTRA_TIME (105)
+
+// 全局变量
+// ===================================================================================
+
+int T; // T：时间片、M：对象的标签数、N：硬盘个数、V：每个硬盘的存储单元个数、G：每个磁头每个时间片最多消耗的令牌数
+int M;
+int N;
+int V;
+int G;
+int TIMESTAMP;
+
+// 结构体定义
+// ===================================================================================
+
+struct Request{
+    int id;                 // 请求 id
+    int objectId;           // 请求的对象 id
+
+    int arriveTime;         // 请求到达的时刻
+    vector<bool> hasRead;   // f(i)：对象的第 i 个块是否读取
+
+    Request(){}
+    Request(int _id, int _objectId, int _arriveTime, int _objectSize){
+        this->id = _id;
+        this->objectId = _objectId;
+        this->arriveTime = _arriveTime;
+        this->hasRead.assign(_objectSize + 1, false);    // 默认未被读
+    }
+};
+
+struct Object{  
+    int id;     // 对象 id
+    int size;   // 对象 size
+    int tagId;    // 对象 tag
+    vector<int> replicaDiskId;                // f(i) = 第 i 个副本的所在磁盘号
+    vector<vector<int>> replicaBlockUnit;   // f(i,j) = 第 i 个副本、第 j 个块所在的磁盘（块）单元号
+
+    queue<Request> requests;                // 未完成的请求队列挂在对象上（用队列，是为了先来先处理）
+
+    Object(){}
+    Object(int _id, int _size, int _tagId){
+        this->id = _id;
+        this->size = _size;
+        this->tagId = _tagId;
+
+        this->replicaDiskId.assign(REP_NUM + 1, 0);
+        this->replicaBlockUnit.assign(REP_NUM + 1, vector<int>(size + 1, 0));
+        this->requests;
+    }
+};
+
+struct Tag{
+    int id;
+    // 管理标签写入磁盘
+    int writeMainDiskId;    // 拥有此 id 标签的对象写入主分区时的磁盘号
+    int writeFreeDiskId;    // 拥有此 id 标签的对象写入空余分区时的磁盘号
+    int writeRandomDiskId;
+    // NOTE: [)
+    int startUnit;          // 此 id 标签的对象在磁盘分区（主分区）上的起始位置
+    int endUnit;            // 此 id 标签的对象在磁盘分区（主分区）上的终止位置
+
+    vector<int> freDel;     // NOTE: 下标从 1 开始
+    vector<int> freWrite;
+    vector<int> freRead;
+    // 非必要不提供默认构造函数。可以使用【默认参数】
+    Tag(/* int& _id,  */int _writeMainDiskId = 1, int _writeFreeDiskId = 1, int _writeRandomDiskId = 1, int _startUnit = 1, int _endUnit = 1) {
+        /* this->id = _id; */
+        this->writeMainDiskId = _writeMainDiskId;
+        this->writeFreeDiskId = _writeFreeDiskId;
+        this->writeRandomDiskId = _writeRandomDiskId;
+        this->startUnit = _startUnit;
+        this->endUnit = _endUnit;
+
+        this->freDel.assign((T - 1) / FRE_PER_SLICING + 2, 0);
+        this->freWrite.assign((T - 1) / FRE_PER_SLICING + 2, 0);
+        this->freRead.assign((T - 1) / FRE_PER_SLICING + 2, 0);
+    }
+    // 计算 writeSpace. NOTE: 前提是 freDel、freWrite、freRead 初始化完成了
+
+    // 得到当前写入的主分区所在磁盘
+    int update_main_disk_id(){
+        int tmp = writeMainDiskId;
+        writeMainDiskId = writeMainDiskId % N + 1;
+        return tmp;
+    }
+
+    // 得到当前写入的空余分区所在磁盘
+    int update_free_disk_id(){
+        int tmp = writeFreeDiskId;
+        writeFreeDiskId = writeFreeDiskId % N + 1;
+        return tmp;
+    }
+
+    // 随机写入的磁盘进行轮转
+    int update_random_disk_id(){
+        int tmp = writeRandomDiskId;
+        writeRandomDiskId = writeRandomDiskId % N + 1;
+        return tmp;
+    }
+};
+
+struct DiskPoint{
+    int position;       // 磁头位置
+    int remainToken;    // 剩余 Token：因为移动需要消耗 Token
+    // 便于计算本次 Token
+    char preAction;     // 上一个动作
+    int preCostToken;   // 上一个消耗的 Token
+
+    DiskPoint(){}
+    DiskPoint(int _position, int _remainToken, char _preAction, int _preCostToken){
+        this->position = _position;
+        this->remainToken = _remainToken;
+        this->preAction = _preAction;
+        this->preCostToken = _preCostToken;
+    }
+};
+
+struct Disk{
+    vector<int> diskUnits;       // MAX_DISK_SIZE = 16384 B
+    DiskPoint diskPoint;    // 4 + 4 + 1 + 4 = 13 B
+
+    Disk(int& _V = V){
+        this->diskUnits.assign(_V + 1, 0);    // 0 代表为空
+        this->diskPoint = DiskPoint(1, 0, 0, 0);
+    }
+};
+
+// 具体数据结构定义
+// ===================================================================================
+
+vector<Object> objects;     // (4 + 4 + 4 + 4*3 + 3*5) * MAX_OBJECT_NUM = 3.9 * 10^7 B ≈ 39 MB
+vector<Tag> tags;
+vector<Disk> disks;                             // MAX_DISK_NUM * (MAX_DISK_SIZE + 13B) ≈ 10 * 16384 = 1.6 * 10^5 ≈ 0.16 MB
---
+++ src/main.cpp
@@ -0,0 +1,366 @@
+#include "global.h"
+
+// init tags
+void pre_process_tags(){
+    for(int i = 1; i < tags.size(); ++i) {
+        tags[i].id = i;
+        tags[i].writeMainDiskId = 1;
+        tags[i].writeFreeDiskId = 1;
+    }
+
+    for (int k = 0; k < 3; ++k){
+        for (int i = 1; i <= M; i++) {
+            for (int j = 1; j <= (T - 1) / FRE_PER_SLICING + 1; j++) {
+                if(k == 0) scanf("%d", &tags[i].freDel[j]);
+                else if(k == 1) scanf("%d", &tags[i].freWrite[j]);
+                else if(k == 2) scanf("%d", &tags[i].freRead[j]);
+            }
+        }
+    }
+    printf("OK\n");
+    fflush(stdout);
+    /* // test
+    /* printf("================================\n");
+    printf("%d %d %d %d %d\n", T, M, N, V, G);
+    for (int k = 0; k < 3; ++k){
+        for (int i = 1; i < tags.size(); i++) {
+            for (int j = 1; j < tags[i].freDel.size(); j++) {
+                if(k == 0) printf("%d ", tags[i].freDel[j]);
+                else if(k == 1) printf("%d ", tags[i].freWrite[j]);
+                else if(k == 2) printf("%d ", tags[i].freRead[j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+    printf("================================\n"); */
+
+    // Do：计算 startUnit、endUnit
+    // 计算每个标签占的空间、计算所有标签占的总容量
+    vector<int> tagSpaces(tags.size());
+    int totalSpace = 0;
+    for (int i = 1; i < tags.size(); ++i){
+        for (int j = 1; j < tags[i].freDel.size(); ++j) {
+            tagSpaces[i] += tags[i].freWrite[j];
+            tagSpaces[i] -= tags[i].freDel[j];
+        }
+        totalSpace += tagSpaces[i];
+    }
+    /* // test
+    for (int i = 1; i < tags.size(); ++i){
+        printf("==============================\n");
+        printf("%d\n", tags[i].writeSpace);
+    } */
+        
+    // 根据每个标签的百分比，计算应该在磁盘上分配的容量；NOTE: 10% 剩余。并计算每个标签的区间
+    vector<int> allocSpaces(tags.size());
+    for (int i = 1; i < tags.size(); ++i){
+        allocSpaces[i] = V * 0.9 * (static_cast<double>(tagSpaces[i]) / totalSpace);
+        tags[i].startUnit = tags[i - 1].endUnit;
+        tags[i].endUnit = tags[i].startUnit + allocSpaces[i];
+    }
+    /* // TEST:
+    for (int i = 1; i < tags.size(); ++i){
+        printf("tag %d's startUnit: %d, endUnit: %d\n", i, tags[i].startUnit, tags[i].endUnit);
+    } */
+}
+
+void delete_one_object(int objectId)
+{
+    Object& object = objects[objectId];
+    for (int i = 1; i <= REP_NUM; ++i){
+        for (int j = 1; j < object.size; ++j){
+            int diskId = object.replicaDiskId[i];
+            int unitId = object.replicaBlockUnit[i][j];
+
+            Disk& disk = disks[diskId];
+            disk.diskUnits[unitId] = 0;
+        }
+    }
+}
+
+void delete_action()
+{
+    // 处理输入
+    static vector<int> deleteObjects(MAX_OBJECT_NUM); // 10^6 * 4 = 4MB
+    int nDelete;
+    scanf("%d", &nDelete);
+    for (int i = 1; i <= nDelete; i++) {
+        scanf("%d", &deleteObjects[i]);
+    }
+
+    // 磁盘上进行删除
+    for (int i = 1; i <= nDelete; i++) {
+        int objectId = deleteObjects[i];
+        delete_one_object(objectId);
+    }
+
+    // 判题机交互（并维护请求数据结构）
+    int abortNum = 0;
+    for (int i = 1; i <= nDelete; ++i){
+        int objcetId = deleteObjects[i];
+        Object& object = objects[objcetId];
+
+        abortNum += object.requests.size();
+    }
+    printf("%d\n", abortNum);
+
+    // 删除请求
+    for (int i = 1; i <= nDelete; ++i){
+        int objcetId = deleteObjects[i];
+        Object& object = objects[objcetId];
+        queue<Request> requests = object.requests;
+
+        int size = requests.size();
+        for (int i = 0; i < size; ++i){
+            Request request = requests.front();
+            requests.pop();
+            printf("%d\n", request.id);
+        }
+    }
+
+    fflush(stdout);
+}
+
+bool try_to_write_main_partition(int diskId, int objectId, int replicaId){
+    vector<int>& diskUnits = disks[diskId].diskUnits;
+    Object& object = objects[objectId];
+    Tag& tag = tags[object.tagId];
+
+    // 副本不能写入重复磁盘
+    for (int i = 1; i <= REP_NUM; ++i){
+        if(object.replicaDiskId[i] == diskId) return false;
+    }
+
+    // 判断主分区剩余空间
+    int restSpace = 0;
+    for (int i = tag.startUnit; i < tag.endUnit; ++i){
+        if(diskUnits[i] == 0) restSpace ++;
+        if(restSpace == object.size) break;
+    }
+    if(restSpace != object.size) return false;
+
+    // 写入磁盘
+    for (int i = tag.startUnit, cnt = 0; i < tag.endUnit && cnt < object.size; ++i){
+        if(diskUnits[i] == 0) {
+            diskUnits[i] = objectId;
+            cnt++;
+
+            // 写入时注意要维护 object 信息
+            object.replicaDiskId[replicaId] = diskId;
+            object.replicaBlockUnit[replicaId][cnt] = i; // 注意 cnt++ 了
+        }  
+    }
+    return true;
+}
+
+bool try_to_write_free_partition(int diskId, int objectId, int replicaId){
+    vector<int>& diskUnits = disks[diskId].diskUnits;
+    Object& object = objects[objectId];
+    Tag& tag = tags[object.tagId];
+
+    // 副本不能写入重复磁盘
+    for (int i = 1; i <= REP_NUM; ++i){
+        if(object.replicaDiskId[i] == diskId) return false;
+    }
+
+    // 判断空余分区剩余空间
+    int restSpace = 0;
+    for (int i = tags[tags.size() - 1].endUnit; i < V + 1; ++i){
+        if(diskUnits[i] == 0) restSpace ++;
+        if(restSpace == object.size) break;
+    }
+    if(restSpace != object.size) return false;
+
+    // 写入磁盘
+    for(int i = tags[tags.size() - 1].endUnit, cnt = 0; i < V + 1 && cnt < object.size; ++i){
+        if(diskUnits[i] == 0) {
+            diskUnits[i] = objectId;
+            cnt++;
+
+            // 写入时注意要维护 object 信息
+            object.replicaDiskId[replicaId] = diskId;
+            object.replicaBlockUnit[replicaId][cnt] = i;
+        }
+    }
+    return true;
+}
+
+bool try_to_write_random_partition(int diskId, int objectId, int replicaId){
+    vector<int>& diskUnits = disks[diskId].diskUnits;
+    Object& object = objects[objectId];
+    Tag& tag = tags[object.tagId];
+
+    // 副本不能写入重复磁盘
+    for (int i = 1; i <= REP_NUM; ++i){
+        if(object.replicaDiskId[i] == diskId) return false;
+    }
+
+    // 判断整块磁盘的剩余空间
+    int restSpace = 0;
+    for (int i = tags[1].startUnit; i < V + 1; ++i){
+        if(diskUnits[i] == 0) restSpace ++;
+        if(restSpace == object.size) break;
+    }
+    if(restSpace != object.size) return false;
+
+    // 写入磁盘：见缝插针
+    for(int i = tags[1].startUnit, cnt = 0; i < V + 1 && cnt < object.size; ++i){
+        if(diskUnits[i] == 0) {
+            diskUnits[i] = objectId;
+            cnt++;
+
+            // 写入时注意要维护 object 信息
+            object.replicaDiskId[replicaId] = diskId;
+            object.replicaBlockUnit[replicaId][cnt] = i;
+        }
+    }
+    return true;
+}
+
+bool write_one_object(int objectId){
+    Object& object = objects[objectId];
+    Tag& tag = tags[object.tagId];
+
+    // 有 3 个副本
+    for (int k = 1; k <= REP_NUM; ++k){
+        // 遍历所有磁盘，尝试写入主分区
+        bool isWriteSucess = false;
+        for (int i = 1; i <= N; ++i) {
+            int writeDiskId = tag.update_main_disk_id();
+            if (try_to_write_main_partition(writeDiskId, objectId, k)) {
+                isWriteSucess = true;
+                break;
+            }
+        }
+        if(isWriteSucess) continue;
+
+        // 遍历所有磁盘，尝试写入空余分区
+        for (int i = 1; i <= N; ++i) {
+            int writeDiskId = tag.update_free_disk_id();
+            if (try_to_write_free_partition(writeDiskId, objectId, k)) {
+                isWriteSucess = true;
+                break;
+            }
+        }
+        if(isWriteSucess) continue;
+
+        // 无奈，只能随机找位置写入
+        for (int i = 1; i <= N; ++i){
+            int writeDiskId = tag.update_random_disk_id();
+            if(try_to_write_random_partition(writeDiskId, objectId, k)){
+                isWriteSucess = true;
+                break;
+            }
+        }
+        assert(isWriteSucess == true);
+    }
+    return true;
+}
+
+void write_action(){
+    // 处理输入
+    static vector<int> writeObjects(MAX_OBJECT_NUM); // 10^6 * 4 = 4MB; 存放 objectId
+    int nWrite;
+    scanf("%d", &nWrite);
+    for (int i = 1; i <= nWrite; ++i){
+        int objectId, objectSize, objectTag;
+        scanf("%d%d%d", &objectId, &objectSize, &objectTag);
+        writeObjects[i] = objectId;
+
+        Object& object = objects[objectId];
+        object.id = objectId;
+        object.size = objectSize;
+        object.tagId = objectTag;
+        object.replicaDiskId.assign(REP_NUM + 1, 0);
+        object.replicaBlockUnit.assign(REP_NUM + 1, vector<int>(object.size + 1, 0));
+    }
+
+    // 写入磁盘
+    for (int i = 1; i <= nWrite; ++i){
+        int objectId = writeObjects[i];
+        write_one_object(objectId);
+    }
+
+    // 与判题机交互
+    for (int i = 1; i <= nWrite; ++i){
+        int objectId = writeObjects[i];
+        Object& object = objects[objectId];
+
+        printf("%d\n", object.id);
+        for (int i = 1; i <= REP_NUM; ++i){
+            printf("%d", object.replicaDiskId[i]);
+            vector<int> blocks = object.replicaBlockUnit[i];
+            for (int j = 1; j <= object.size; j++){
+                printf(" %d", blocks[j]);
+            }
+            printf("\n");
+        }
+    }
+
+    fflush(stdout);
+}
+
+void read_action()
+{
+    int nRead;
+    int requestId, objectId;
+    scanf("%d", &nRead);
+    for (int i = 1; i <= nRead; i++) {
+        scanf("%d%d", &requestId, &objectId);
+
+        Object& object = objects[objectId];
+        queue<Request>& requests = object.requests;
+
+        Request request;
+        request.id = requestId;
+        request.objectId = objectId;
+        request.arriveTime = TIMESTAMP;
+        request.hasRead = vector<bool>(object.size + 1, false);
+        object.requests.push(request);
+    }
+    
+    for (int i = 1; i < disks.size(); ++i){
+        printf("#\n");
+    }
+
+    int finishRequestNum = 0;
+    printf("%d\n", finishRequestNum);
+
+    fflush(stdout);
+}
+
+
+void timestamp_action()
+{
+    scanf("%*s%d", &TIMESTAMP);
+    printf("TIMESTAMP %d\n", TIMESTAMP);
+
+    fflush(stdout);
+}
+
+int main()
+{
+    scanf("%d%d%d%d%d", &T, &M, &N, &V, &G);
+
+    // 初始化全局变量（vector 分配空间）
+    tags.assign(M + 1, Tag());              // Tag 没有默认构造函数，使用默认参数
+    objects.resize(MAX_OBJECT_NUM + 1);     // 待留写入时初始化每个 object 对象
+    disks.assign(N + 1, Disk());
+
+
+    pre_process_tags();
+    
+    // for (int i = 1; i <= N; i++) {
+    //     disk_point[i] = 1;
+    // }
+
+    for (int t = 1; t <= T + EXTRA_TIME; t++) {
+        timestamp_action();
+        delete_action();
+        write_action();
+        read_action();
+    }
+
+    return 0;
+}
